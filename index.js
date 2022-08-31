1a059889dab978c5f5a7e2f8bb5946986e8fdc6a
Umar <gadaevduk@gmail.com>
2022-08-31 09:59:08 +0300

    создал первый объект с характеристиками солдата

---
+++ index.js
@@ -0,0 +1,19 @@
+const soldier = {
+    userName: '',
+    health: 10,
+    weapon:{
+        name: 'ak-47',
+        cartridges: 30
+    },
+    supplies: 3,
+   fire(){
+
+   },
+   recharge(){
+
+   },
+   hurt(){
+
+   }
+
+}
