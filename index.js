aff8358e43152ef0c2d3a35968df6528c381d39e
Umar <gadaevduk@gmail.com>
2022-08-31 10:16:00 +0300

    Сделал функционал игры

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const soldier = {
-    userName: '',
+    userName: 'Umar',
     health: 10,
     weapon:{
         name: 'ak-47',
         cartridges: 30
     },
     supplies: 3,
-   fire(){
+   fire(){ 
+    this.weapon.cartridges--
+    console.log("бах-бах")
+    if(this.weapon.cartridges === 0){
+        return console.log("обойма пуста. Перезаредитесь")
+    }
 
    },
    recharge(){
+    this.weapon.cartridges = 30;
+    this.supplies--;
+    console.log("Перезарядка...")
+    if(this.supplies === 0){
+        console.log('не осталось припасов!')
+    }
 
    },
    hurt(){
-
+    this.health--
+    if(this.health === 0){
+        console.log('ТЫ ПРОИГРАЛ!')
+    }
    }
 
 }
+soldier.fire()
